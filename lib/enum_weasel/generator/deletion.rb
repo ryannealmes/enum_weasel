26656715966287d7098f739b0cec2c3978adce6a
Ryan-Neal Mes <ryan.mes@gmail.com>
2017-02-05 17:15:16 +0200

    Added class to delete all enum tables based on source and database enum tables

---
+++ lib/enum_weasel/generator/deletion.rb
@@ -0,0 +1,27 @@
+module EnumWeasel
+  class Generator
+    class Deletion
+      attr_reader :source_enum_tables, database_enum_tables
+
+      def initialize(source_enum_tables, database_enum_tables)
+        @source_enum_tables = source_enum_tables
+        @database_enum_tables = database_enum_tables
+      end
+
+      def call
+        enums_to_delete = database_enum_tables - source_enum_tables 
+        enums_to_delete.each { |table_name| delete_table table_name }
+      end
+
+      def delete_table table
+        if connection.table_exists?(table_name)
+          connection.drop table table_name
+        end
+      end
+
+      def connection
+        ActiveRecord::Base.connection
+      end
+    end
+  end
+end
