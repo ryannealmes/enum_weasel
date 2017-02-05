45dcda8a3e7551bcfcdcd1f3da01719d6e2f457d
Andrew Tainton <andrew@getsmarter.co.za>
2017-02-05 16:54:17 +0200

    Added methods for getting new enums and exisiting tables

--- lib/enum_weasel/generator.rb
+++ lib/enum_weasel/generator.rb
@@ -1,8 +1,24 @@
 module EnumWeasel
   class Generator
+
     def call
-      puts ::ActiveRecord::Base.connection.table_exists?("weasel")
       return "pong"
+    end
+
+    def model_enums
+      @model_enums ||= EnumWeasel::GetModelEnums.new.call
+    end
+
+    def existing_tables
+      @existing_tables ||= ActiveRecord::Base.connection.tables.select { |key, value| key.to_s.match(/^enum/) }
+    end
+
+    def new_enum_tables
+      model_enums.except!(*existing_tables)
+    end
+
+    def to_be_deleted_enum_tables
+      existing_tables.except!(*model_enums.keys)
     end
   end
 end
